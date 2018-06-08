b06ad79125c58dd86daeb59bffef937d4541aa43
Shuchin_P <pshuchin@yahoo.com>
2018-06-08 21:45:19 +0530

    Initial Commit Login and UserIndex Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserindexComponent } from './userindex/userindex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserindexComponent
   ],
   imports: [
     BrowserModule
